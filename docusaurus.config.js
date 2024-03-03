495ba5848f729c647b87b70505ee42b8677c85bf
Qi Zhang <zhangqi444@gmail.com>
2024-03-03 14:01:11 -0800

    warn for onBrokenLinks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   projectName: 'open-saas', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
